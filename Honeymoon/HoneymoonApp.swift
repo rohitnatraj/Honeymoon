f94b6e9592487ea704f7053c275244b133652131
Rohit Natraj <rohitnatraj@gmail.com>
2021-11-15 13:52:56 -0700

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Natraj, Rohit on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
